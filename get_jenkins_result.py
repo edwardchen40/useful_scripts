83877b8c5bc5de6819cae5f72af80ce50e7f1550
Edward <edwardchen40git@gmail.com>
2016-05-06 18:36:10 +0800

    Create get_jenkins_result.py
    Check build status of Jenkins from the command line

---
+++ get_jenkins_result.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import json 
+import sys 
+import urllib2
+
+jenkinsUrl = "http://192.168.5.1:8080/job/"
+#jenkinsUrl = "https://192.168.5.1:8080/job/Autotest/lastBuild/api/json"
+
+
+if len( sys.argv ) > 1 : 
+    jobName = sys.argv[1]
+else :
+    sys.exit(1)
+
+try:
+    jenkinsStream = urllib2.urlopen( jenkinsUrl + jobName + "/lastBuild/api/json" )
+    
+except urllib2.HTTPError, e:
+    print "URL Error: " + str(e.code) 
+    print "      (job name [" + jobName + "] probably wrong)"
+    sys.exit(2)
+
+try:
+    buildStatusJson = json.load( jenkinsStream )
+except:
+    print "Failed to parse json"
+    sys.exit(3)
+
+if buildStatusJson.has_key( "result" ):    
+    print "[" + jobName + "] build status: " + buildStatusJson["result"]
+    if buildStatusJson["result"] != "SUCCESS" :
+        exit(4)
+    else:
+        sys.exit(5)
+
+sys.exit(0)

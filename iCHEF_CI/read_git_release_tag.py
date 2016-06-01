673895621c539c08190413ea1b9e33bac8d33b7d
Edward <edwardchen40git@gmail.com>
2016-06-01 19:26:40 +0800

    Update read_git_release_tag.py

--- iCHEF_CI/read_git_release_tag.py
+++ iCHEF_CI/read_git_release_tag.py
@@ -1,30 +1,40 @@
-#!/usr/bin/env python
-from subprocess import call
-#import pdb
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+from subprocess import call
+import os
+
+#import pdb
 #pdb.set_trace()
-call('git tag -l 2.5.0.* > newtag.txt' ,shell=True) 
-# with an args list and shell=False (the default) does not use a shell.
+call("cp ~/OldTag.txt ~/iCHEF-2.0/", shell=True)
+#call("cp ~/OldTag.txt ~/iCHEF-2.0/", shell=True)
+#call("cp ~/read_release_tag.py ~/iCHEF-2.0/", shell=True)
+call("git tag -l 2.5.* > NewTag.txt", shell=True)
 
 # Read old tags from old tag file
-linesOld = [line.rstrip('\n') for line in open('oldtag.txt')]
+linesOld = [line.rstrip('\n') for line in open('OldTag.txt')]
 oldTag = linesOld[-1]
 
 # Read latest tags from latest file
-linesNew = [line.rstrip('\n') for line in open('newtag.txt')]
+linesNew = [line.rstrip('\n') for line in open('NewTag.txt')]
 newTag = linesNew[-1]
 
 # Compare the tags between old and latest tag files
 
 if oldTag != newTag :
-	call(['git', 'checkout', 'tags/' + newTag])
-	call(['mv', 'newtag.txt', 'oldtag.txt'])	
-	print "================================================"
-	print "= Now the release branch tag is" + newTag + " ="
-	print "================================================"
-	call('~/auto_build.sh', shell=True)
-	call('~/auto_deploy.sh',shell=True)
+    call("cp NewTag.txt ~/OldTag.txt", shell=True)
+    call(['git', 'add', '-A'])
+    call(['git', 'commit', '-am', '\"There will be checkout to another brance.\"'])
+    call(['git', 'checkout', 'tags/' + newTag])
+    print "================================================"
+    print "= Now the release branch tag is" + newTag + " ="
+    print "================================================"
+#    call("cd ~;cp ~/tmp_files/Appfile ~/iCHEF-2.0/fastlane/")
+#    call("cd ~;cp ~/tmp_files/Fastfile ~/iCHEF-2.0/fastlane/")
+#    call('~/auto_build.sh', shell=True)
+#    call('~/auto_deploy.sh',shell=True)
 else:
-	print "========================================"
-	print "= Here is not a latest release version ="
-	print "========================================"
+    print "========================================"
+    print "= Here is not a latest release version ="
+    print "========================================"
+

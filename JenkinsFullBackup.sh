20ab7b43473dc13694ee43113dcd2a3c36913e3d
Edward <edwardchen40git@gmail.com>
2016-07-28 13:38:05 +0800

    Update JenkinsFullBackup.sh

--- JenkinsFullBackup.sh
+++ JenkinsFullBackup.sh
@@ -19,5 +19,18 @@ cd $JENKINS_HOME
 
 # backup workspace need lot of diskspace, you can ignore like
 # --exclude=./workspace
-tar zcvf $JENKINS_BACKUP_HOME/$JENKINS_BACKUP_FILE ./* \
-    --exclude=./war
+tar zcvf $JENKINS_BACKUP_HOME/$JENKINS_BACKUP_FILE ./* 
+
+# You can add scripts to crontab
+# crobtab -e
+# paste "0 12 * * 6 root /bin/sh ~/JenkinsFullBackup.sh > /dev/null 2>&1"
+# "crontab -l" to check 
+
+
+#Restore jenkins steps
+#cd .jenkins
+#cp /User/admin/ci_backup/jenkins-backup.2016-10-05.tar.gz ./
+#tar zxvf ./jenkins-backup.2016-10-05.tar.gz
+#rm ./jenkins-backup.2016-10-05.tar.gz
+#chown -R jenkins:jenkins .jenkins
+#service jenkins restart

04be04715b44b95a53d7a75af723055462d7292b
Edward <edwardchen40git@gmail.com>
2017-04-13 18:50:26 +0800

    Create get_jenkins_result_committer_requests.py

---
+++ get_jenkins_result_committer_requests.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+import json 
+import sys 
+import urllib2
+import base64
+import subprocess
+import requests
+
+jenkinsUrl = "http://10.56.141.23:8080/job/"
+username = "edward.chen"
+password = "!QAZxsw2"
+iftttUrl = "https://maker.ifttt.com/trigger/gln_healthcheck/with/key/bI7N2-FGqNnpMAouTSi2AU"
+
+if len( sys.argv ) > 1 : 
+    jobName = sys.argv[1]
+else :
+    print "Please supply a job name from Jenkins server." 
+    print "EX: python get_jenkins_result.py GLN_automatic_Health_check_Beta"
+    sys.exit(1)
+
+# Step 1 - Connect to Jenkons then get json file
+try:
+    #jenkinsStream = urllib2.urlopen( jenkinsUrl + jobName + "/lastBuild/api/json" ) # No user login
+    request = urllib2.Request(jenkinsUrl + jobName + "/lastBuild/api/json") # User login method
+    base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+    request.add_header("Authorization", "Basic %s" % base64string)   
+    result = urllib2.urlopen(request)
+
+except urllib2.HTTPError, e:
+    print "URL Error: " + str(e.code) 
+    print "      (job name [" + jobName + "] probably wrong)"
+    sys.exit(2)
+
+
+# Step 2 - Query final committer
+committerName = subprocess.check_output("GIT_NAME=$(git --no-pager show -s --format='%an' $GIT_COMMIT)&&echo $GIT_NAME", shell=True)
+
+# Step 3 - Get Json file
+try:
+    #buildStatusJson = json.load( jenkinsStream )
+    buildStatusJson = json.load( result )
+except:
+    print "Failed to parse json"
+    sys.exit(3)
+
+# Step 4 - Prepare sending value you want
+fullDisplayName = buildStatusJson["fullDisplayName"]
+fullUrlPath = buildStatusJson["url"]
+
+# Debug 
+#print json.dumps(buildStatusJson, indent=4, sort_keys=True)
+#for key, value in buildStatusJson.items():
+   # print("key:" + key)
+   
+# Step 5 - Send request to IFTTT
+if buildStatusJson.has_key( "result" ): 
+    #print json.dumps(buildStatusJson, indent=4, sort_keys=True) 
+    #print "[" + jobName + "] build status: " + buildStatusJson["result"]
+    if buildStatusJson["result"] != "SUCCESS" :
+        headers = {"Content-Type": "application/json"}
+        data = {"value1": fullDisplayName, "value2": committerName, "value3": fullUrlPath}
+        response = requests.post(url=iftttUrl, json=data, headers=headers)
+        print (response.status_code , response.reason )
+        exit(4)
+    else:
+        sys.exit(5)
+
+sys.exit(0)

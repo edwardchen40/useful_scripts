ec75149313c514e89fd811d29d82bfcff31ee27f
Edward <edwardchen40git@gmail.com>
2018-01-10 14:21:44 +0800

    Update Rerun_failed_test.sh

--- Rerun_failed_test.sh
+++ Rerun_failed_test.sh
@@ -1,76 +1,72 @@
 #!/bin/sh
+
 # This script file is to re-execute test cases and merge test result on Robot framework
 # usage: ./run_robot.sh any_robot_command test_suite
 # note: cannot execute command within ', ex. -t 'test case'
 # Reference: http://laurent.bristiel.com/re-executing-failed-test-cases-and-merging-outputs-with-robot-framework/
 
+UPDATE_TESTRAIL_SCRIPT="python ../common/testrail/update_robot_test_result_to_testrail.py --folder=$RESULT_REPORT --pid=4 --user=jpt0070@linecorp.com --pwd='line12#$' --runid=${GLN_RUN_ID}"
+
 # clean previous output files
-rm -f report/output.xml
-rm -f report/rerun.xml
-rm -f report/first_run_log.html
-rm -f report/second_run_log.html
-rm -f report/xunit.xml
+rm -rf $RESULT_REPORT
 
-run_with_tag=''
-
-if [ ! -z "$TAG" ]; then
-    run_with_tag='-i '$TAG
+if [ -z "$NUMBER_OF_PROCESSOR" ]; then
+    NUMBER_OF_PROCESSOR=1
 fi
  
 echo
+STARTTIME=$(date +%s)
+echo $STARTTIME
 echo "#######################################"
 echo "# Running portfolio a first time      #"
 echo "#######################################"
 echo
-pybot $run_with_tag -x xunit.xml --outputdir report $@
+pabot --processes $NUMBER_OF_PROCESSOR -d $RESULT_REPORT -e retire -L debug $@
 res=$?
 
 # we stop the script here if all the tests were OK
 if [ $res -eq 0 ]; then
-    echo "we don't run the tests again as everything was OK on first try"
-    exit 0  
+    echo "The first run is done."
+    echo $UPDATE_TESTRAIL_SCRIPT
+    exit 0
 fi
 # otherwise we go for another round with the failing tests
- 
-# we keep a copy of the first log file
-cp report/log.html  report/first_run_log.html
- 
+
 # we launch the tests that failed
 echo
+date
 echo "#######################################"
 echo "# Running again the tests that failed #"
 echo "#######################################"
 echo
-pybot --outputdir report --rerunfailed report/output.xml -x xunit.xml --output rerun.xml $@
-res=$?
-# Robot Framework generates file rerun.xml
 
-if [ $res -ne 0 ]; then
-    RES=true
-fi
- 
-# we keep a copy of the second log file
-cp report/log.html  report/second_run_log.html
- 
+pabot --processes $NUMBER_OF_PROCESSOR -d $RESULT_REPORT --rerunfailed $RESULT_REPORT/output.xml --output rerun.xml -e retire -L debug $@
+res=$?
+
+ENDTIME=$(date +%s)
+echo "It takes $(($ENDTIME - $STARTTIME)) seconds to complete this task..."
+
 # Merging output files
 echo
+date
 echo "########################"
 echo "# Merging output files #"
 echo "########################"
 echo
-rebot --nostatusrc --outputdir report --output output.xml --merge report/output.xml  report/rerun.xml
 # Robot Framework generates a new output.xml
+rebot --nostatusrc --outputdir $RESULT_REPORT --output output.xml --merge $RESULT_REPORT/output.xml  $RESULT_REPORT/rerun.xml
 
-if [ $RES ]; then
-    exit 1  
+if [ $res -eq 0 ]; then
+    echo "The second re-run is done."
+    echo $UPDATE_TESTRAIL_SCRIPT
+    exit 0
 fi
-
-
 
 
 
 // ===== 分隔線 ====
 // In Jenjins , Execute Shell
+
 CHROMEDRIVER_ABS_PATH=`cd util-lib; pwd`
 export PATH=$PATH:$CHROMEDRIVER_ABS_PATH:/usr/local/bin
 

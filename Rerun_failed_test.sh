12710f9f0300771453a83f96a7506466f573c405
Edward <edwardchen40git@gmail.com>
2018-02-16 13:36:43 +0800

    delete jenkins example

--- Rerun_failed_test.sh
+++ Rerun_failed_test.sh
@@ -61,33 +61,3 @@ if [ $res -eq 0 ]; then
     echo $UPDATE_TESTRAIL_SCRIPT
     exit 0
 fi
-
-
-
-// ===== 分隔線 ====
-// In Jenjins , Execute Shell
-
-CHROMEDRIVER_ABS_PATH=`cd util-lib; pwd`
-export PATH=$PATH:$CHROMEDRIVER_ABS_PATH:/usr/local/bin
-
-set +e
-
-cd gln
-
-export RESULT_REPORT=pc_report_remote
-export NUMBER_OF_PROCESSOR=3
-./glbot ${tag} -V env/${env}.py:TW -v DELAY:0.6 -v VALID_USER:jpt0070 -v VALID_PASSWORD:line12#$ -e timeline pc/
-
-export RESULT_REPORT=pc_report_timeline
-export NUMBER_OF_PROCESSOR=1
-./glbot ${tag} -V env/${env}.py:TW -v DELAY:0.6 -v VALID_USER:jpt0070 -v VALID_PASSWORD:line12#$ -i timeline pc/
-
-#pabot --processes 4 -d pc_report_remote -V env/${env}.py:TW -v DELAY:1 -v VALID_USER:jpt0070 -v VALID_PASSWORD:'line12#$' -e retire -e timeline ${tag} -L debug pc/
-#pybot -d pc_report_timeline -V env/${env}.py:TW -v DELAY:0.5 -v VALID_USER:jpt0070 -v VALID_PASSWORD:'line12#$' -i timeline -L debug pc/
-
-set -e
-
-rebot --outputdir pc_report --output output.xml pc_report_remote/output.xml pc_report_timeline/output.xml
-
-#cd ..
-#python common/testrail/update_robot_test_result_to_testrail.py --folder=gln/pc_report --pid=4 --user=jpt0070@linecorp.com --pwd='line12#$' --runid=${GLN_RUN_ID}
